9f0b06f3854df686bea65d88f4eb9f2b4352d912
jsxtam <tam.ja@husky.neu.edu>
2020-04-10 17:41:13 -0400

    loaded data

--- js/visualization.js
+++ js/visualization.js
@@ -4,4 +4,16 @@
 
   console.log("Hello, world!");
 
+  // Load the data from a json file
+  d3.csv("/data/Girls aged 15-19 Marriage.csv", (data1) => {
+    d3.csv("/data/Women aged 20-24 Child-Marriage.csv", (data2) => {
+      d3.csv("/data/Women aged 20-24 Marriage.csv", (data3) => {
+        // ensure data is loaded correctly
+        console.log(data1);
+        console.log(data2);
+        console.log(data3);
+      });
+    });
+  });
+
 })());
