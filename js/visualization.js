fb985e0752bf602cdf5ce4f2ecb057f3b03e9bd9
jsxtam <tam.ja@husky.neu.edu>
2020-04-13 23:23:57 -0400

    scatterplot reusable chart framwork

--- js/visualization.js
+++ js/visualization.js
@@ -5,15 +5,27 @@
   console.log("Hello, world!");
 
   // Load the data from a json file
-  d3.csv("/data/Girls aged 15-19 Marriage.csv", (data1) => {
-    d3.csv("/data/Women aged 20-24 Child-Marriage.csv", (data2) => {
-      d3.csv("/data/Women aged 20-24 Marriage.csv", (data3) => {
-        // ensure data is loaded correctly
-        console.log(data1);
-        console.log(data2);
-        console.log(data3);
-      });
-    });
+  d3.csv("/data/Women aged 20-24 Child-Marriage.csv", (data) => {
+
+    // ensure data is loaded correctly
+    console.log(data);
+    console.log(data[1]["Married before Age 18"]);
+
+    // General event type for selections, used by d3-dispatch
+    // https://github.com/d3/d3-dispatch
+    const dispatchString = "selectionUpdated";
+
+    // create a scatterplot
+    let spMarriageBirth = scatterplot()
+      .x(data => data["Married before Age 18"])
+      .xLabel("Married before Age 18")
+      .y(data => data["Gave birth Before Age 18"])
+      .yLabel("Gave birth Before Age 18")
+      .selectionDispatcher(d3.dispatchString)
+      ("#scatterplot", data);
+        
+
+      // add brusing and linking here
   });
 
 })());
